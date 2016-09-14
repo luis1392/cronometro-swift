0c2ebecff839298b640ca87a65c66845340065f6
Luis Enrique Tierrafria Rodriguez <luis@luiss-imac.lan>
2016-09-14 00:50:32 -0500

    Initial Commit

---
+++ Cronometro/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Cronometro
+//
+//  Created by Luis Enrique Tierrafria Rodriguez on 9/14/16.
+//  Copyright © 2016 Luis Enrique Tierrafria Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
