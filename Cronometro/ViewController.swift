542164714677a19cac3696b51251a827bce86509
Luis Enrique Tierrafria Rodriguez <luis@Luiss-iMac.lan>
2016-11-21 01:22:28 -0600

    end

--- Cronometro/ViewController.swift
+++ Cronometro/ViewController.swift
@@ -10,6 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var time: UILabel!
+    
+    var timer = NSTimer()
+    var count = 0
+    
+    @IBAction func pause(sender: AnyObject) {
+        timer.invalidate()
+    }
+    
+    
+    @IBAction func stop(sender: AnyObject) {
+        timer.invalidate()
+        time.text = "0"
+        count = 0
+    }
+    
+    func updateTime() {
+        count+=1
+        time.text = "\(count)"
+        
+    }
+    
+    @IBAction func play(sender: AnyObject) {
+        timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: #selector(ViewController.updateTime), userInfo: nil, repeats: true)    }
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
